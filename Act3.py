59f78905cb944293a062ef1bc670665ed43dd449
martha <115857319+marthabacoy@users.noreply.github.com>
2023-11-11 00:09:58 +0800

    Geolocation Enhancements

--- Act3.py
+++ Act3.py
@@ -23,42 +23,56 @@ def login():
         
 def get_public_ip_info():
     try:
-        # get request to ipinfo.io API to retrieve information
-        response = requests.get('https://ipinfo.io/json')
+        #user authentication
+        if login():
+            # Request geolocation information from the ipinfo.io API
+            response = requests.get('https://ipinfo.io/json')
+            ipstack_api_key = 'YOUR_IPSTACK_API_KEY'  # Replace with your actual IPStack API key
 
-        # check if the http response status code is 200
-        if response.status_code == 200:
-            data = response.json()
-            
-            # extract and print ipv4 address
-            print(f'Public IPv4 Address: {data["ip"]}')
+            #check if the HTTP response status code is successful (200)
+            if response.status_code == 200:
+                data = response.json()
 
-            # check and print if ipv6 is available
-            if 'ip6' in data:
-                print(f'Public IPv6 Address: {data["ip6"]}')
+                #extract and print ipv4 address
+                print(f'Public IPv4 Address: {data["ip"]}')
+
+                #check and print if ipv6 is available
+                if 'ip6' in data:
+                    print(f'Public IPv6 Address: {data["ip6"]}')
+                else:
+                    print('IPv6 Address not available')
+
+                #extract and print detailed geolocation information
+                loc = data.get("loc", "").split(",")
+                latitude = loc[0] if loc else "N/A"
+                longitude = loc[1] if len(loc) > 1 else "N/A"
+                print(f'Latitude: {latitude}')
+                print(f'Longitude: {longitude}')
+                print(f'Geolocation: {data["city"]}, {data["region"]}, {data["country"]}')
+
+                # Get continent information from ipstack.com
+                continent = get_continent_info(data["ip"], ipstack_api_key)
+                print(f'Continent: {continent}')
+
+                print(f'Postal Code: {data["postal"]}')
+                print(f'Time Zone: {data["timezone"]}')
+                print(f'ISP: {data["org"]}')
+
+                #check if ASN (Autonomous System Number) is available
+                if 'asn' in data:
+                    print(f'ASN: {data["asn"]}')
+                else:
+                    print('ASN: Information not available')
+
+                #extract and print country code
+                print(f'Country Code: {data["country"]}')
+
+                #log ip address for history tracking
+                log_ip_address(data["ip"])
+
             else:
-                print('IPv6 Address not available')
+                print(f'Failed to retrieve data. Status code: {response.status_code}')
 
-            # extract and print geolocation information
-            loc = data.get("loc", "").split(",")
-            latitude = loc[0] if loc else "N/A"
-            longitude = loc[1] if len(loc) > 1 else "N/A"
-            print(f'Latitude: {latitude}')
-            print(f'Longitude: {longitude}')
-            print(f'Geolocation: {data["city"]}, {data["region"]}, {data["country"]}')
-            print(f'ISP: {data["org"]}')
-            
-            # check if asn is available
-            if 'asn' in data:
-                print(f'ASN: {data["asn"]}')
-            else:
-                print('ASN: Information not available')
-                
-            #extract and print country code
-            print(f'Country Code: {data["country"]}')
-        else:
-            print(f'Country Code: Failed to retrieve data. Status code: {response.status_code}')
-    
     except Exception as e:
         print(f'An error occurred: {str(e)}')
 
